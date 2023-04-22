ae5b7c81a49331540a48f78e849ca6afbb9adcef
franc333s <marinafase@gmail.com>
2023-04-22 12:54:08 +0200

    Feature accordion and hovers

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
 
 // Carousel in hero
-function carouselNewsletter(selector, speed) {
-    const parentSelector = document.querySelector(selector);
-    const clone = parentSelector.innerHTML;
-    const firstElement = parentSelector.children[0];
-    let i = 0;
-    
-    parentSelector.insertAdjacentHTML('beforeend', clone);
-    parentSelector.insertAdjacentHTML('beforeend', clone);
-    
-    setInterval(function () {
-      console.log('aloha');
-      firstElement.style.marginLeft = `-${i}px`;
-      if (i > firstElement.clientWidth) {
-        i = 0;
-      }
-      i = i + speed;
-    }, 0);
-  }
-   
-  window.addEventListener('load', carouselNewsletter('.jsCarouselNewsletter', .2))
-    
+function carouselTitle(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
   
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+  
+  setInterval(function () {
+    firstElement.style.marginLeft = `-${i}px`;
+    if (i > firstElement.clientWidth) {
+      i = 0;
+    }
+    i = i + speed;
+  }, 0);
+}
+ 
+window.addEventListener('load', carouselTitle('.jsCarouselTitle', .4))
+  
+
+
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
