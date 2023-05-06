2476780def17dba979c9befd1cc1860078f284ac
franc333s <marinafase@gmail.com>
2023-05-06 15:41:46 +0200

    Footer added

--- js/main.js
+++ js/main.js
@@ -37,25 +37,13 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-
-// Mas info
-
-// Mobile menu - opening/closing from Menu button
+// hide hover image
 const jsBtn = document.querySelector('.jsBtn');
 jsBtn.addEventListener('click', toggleMenu);
 
 const jsHide = document.querySelector('.jsHide');
 
 function toggleMenu() {
+    console.log('aloha');
     jsHide.classList.toggle('jsHide')
 }
-
-// // Mobile menu - closing from green layer
-// const jsList = document.querySelector('.jsList');
-// jsList.addEventListener('click', hideMenu);
-
-// const closeMenu = document.querySelector('.jsList');
-
-// function hideMenu() {
-//     closeMenu.classList.toggle('jsHide')
-// }
